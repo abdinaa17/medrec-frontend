077f3d4ae2a6de1779232585550b66ce8e07a666
abdinaa17 <abdinassir586@gmail.com>
2025-09-18 22:24:56 -0400

    Added the medication admin record

--- src/pages/SingleConsumer.tsx
+++ src/pages/SingleConsumer.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import LoadingSpinner from "../components/LoadingSpinner";
 import Message from "../components/Message";
 import type { Medication } from "../types/Medication";
+import { getDaysInAMonth } from "../utils/date";
+import { ListGroup, Table, Form } from "react-bootstrap";
 
 const SingleConsumer = () => {
   const { id } = useParams();
@@ -13,6 +15,10 @@ const SingleConsumer = () => {
   const [medications, setMedications] = useState<Medication[]>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null | any>();
+
+  const daysInMonth = getDaysInAMonth();
+
+  console.log(daysInMonth);
 
   const fetchSingleConsumer = async () => {
     setIsLoading(true);
@@ -60,16 +66,73 @@ const SingleConsumer = () => {
       </h2>
       <p>{singleConsumer?.dateOfBirth}</p>
 
+      <p>No known Allergies</p>
       <div>
-        Medication list
+        <h2>Medication list</h2>
         <div>
           {medications && medications.length > 0
             ? medications.map((medication) => {
                 return (
-                  <ul key={medication.id}>
-                    <li>{medication.name}</li>
-                    <li>{medication.dose}</li>
-                  </ul>
+                  <div key={medication.id}>
+                    <ListGroup>
+                      <ListGroup.Item>
+                        {medication.name} {medication.dose}
+                      </ListGroup.Item>
+                    </ListGroup>
+                    <Table striped bordered hover size="sm">
+                      <thead>
+                        <tr>
+                          <th>Time</th>
+                          {Array.from({ length: daysInMonth }).map((_, i) => {
+                            return (
+                              <th key={i + 1} role="button">
+                                {i + 1}
+                              </th>
+                            );
+                          })}
+                        </tr>
+                        <tr>
+                          <th>7am</th>
+                          {Array.from({ length: daysInMonth }).map((_, i) => {
+                            return (
+                              <th key={i + 1} role="button">
+                                <Form.Select size="sm">
+                                  <option value="AA">Administered</option>
+                                  <option value="R">Refused</option>
+                                </Form.Select>
+                              </th>
+                            );
+                          })}
+                        </tr>
+                        <tr>
+                          <th>3pm</th>
+                          {Array.from({ length: daysInMonth }).map((_, i) => {
+                            return (
+                              <th key={i + 1} role="button">
+                                <Form.Select size="sm">
+                                  <option value="AA">Administered</option>
+                                  <option value="R">Refused</option>
+                                </Form.Select>
+                              </th>
+                            );
+                          })}
+                        </tr>
+                        <tr>
+                          <th>7pm</th>
+                          {Array.from({ length: daysInMonth }).map((_, i) => {
+                            return (
+                              <th key={i + 1} role="button">
+                                <Form.Select size="sm">
+                                  <option value="AA">Administered</option>
+                                  <option value="R">Refused</option>
+                                </Form.Select>
+                              </th>
+                            );
+                          })}
+                        </tr>
+                      </thead>
+                    </Table>
+                  </div>
                 );
               })
             : "No medication found"}
---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+export const getDaysInAMonth = (): number => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+
+  const month = now.getMonth() + 1;
+
+  return new Date(year, month, 0).getDate();
+};
